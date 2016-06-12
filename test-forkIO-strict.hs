7e4f010de52e47a07d3180e844ac0dbac97ac41b
jwin <jwin1968@gmail.com>
2016-06-12 22:43:27 +0200

    force and strict

---
+++ test-forkIO-strict.hs
@@ -0,0 +1,56 @@
+
+import System.IO
+import Control.Concurrent (threadDelay, forkIO, myThreadId)
+import Control.Exception (evaluate, bracket_)
+import Control.Concurrent.Async (async, wait, cancel)
+import Control.Concurrent.MVar
+import Control.DeepSeq
+
+import Debug.Trace (traceEventIO)
+import GHC.Conc (labelThread)
+
+import Data.Sequence
+
+-- Intentionally slow fib
+fib :: Integer -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+blip :: IO ()
+blip = do
+  tid <- myThreadId
+  labelThread tid "blip-thread"
+  doBlips where
+    doBlips = do
+      putStrLn "BLIP"
+      threadDelay 500000
+      doBlips
+
+
+main :: IO ()
+main = do
+  hSetBuffering stdin NoBuffering
+  tid <- myThreadId
+  labelThread tid "main-thread"
+
+  v1 <- newEmptyMVar
+
+  forkIO $ do
+    tid <- myThreadId
+    labelThread tid "fib-thread"
+
+    let r1 = force $ map fib [20, 29, 30, 34, 36]
+    putMVar v1 $! r1
+
+    -- let r1 = map fib [20, 29, 30, 34]
+    -- print r1
+    -- putMVar v1 r1
+
+  blips <- async blip
+
+  l1 <- takeMVar v1
+  print l1
+
+  cancel blips
+  return ()
